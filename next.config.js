e3dfde8712efc1e6cead183aa8cbe3c726fb43b0
Camilo arias <camillo47@gmail.com>
2021-07-26 23:46:01 -0500

    add Facebook domain for user images to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos', 'lh3.googleusercontent.com'],
+    domains: ['picsum.photos', 'lh3.googleusercontent.com', 'graph.facebook.com'],
   },
 }
