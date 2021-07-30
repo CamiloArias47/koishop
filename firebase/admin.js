5b2c9caf64ed557a1ba6609e69f77373bebfe144
Camilo arias <camillo47@gmail.com>
2021-07-30 02:33:27 -0500

    Chamge import of env variables

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,9 +1,17 @@
 var admin = require("firebase-admin");
 
-const firebaseConfig = JSON.parse(process.env.FIRABESE_ADMIN)
-
-console.log({firebaseConfig})
-console.log(typeof firebaseConfig)
+const firebaseConfig = {
+    "type": process.env.FIRABESE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_private_key_id,
+    "private_key": process.env.FIREBASE_private_key,
+    "client_email": process.env.FIREBASE_client_email,
+    "client_id": process.env.FIREBASE_client_id,
+    "auth_uri": process.env.FIREBASE_auth_uri,
+    "token_uri": process.env.FIREBASE_token_uri,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_auth_provider_x509_cert_url,
+    "client_x509_cert_url": process.env.FIREBASE_client_x509_cert_url
+}
 
 try {
   admin.initializeApp({
