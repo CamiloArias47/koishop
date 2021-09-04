727e4de92ee05d7bd73bfeee8f27aa8aca3227d2
Camilo arias <camillo47@gmail.com>
2021-09-04 12:05:30 -0500

    pay page / scroll table 15.5%

--- styles/style-pago.js
+++ styles/style-pago.js
@@ -7,6 +7,20 @@ export default css`
     overflow-x:auto;
 }
 
+.wraper-table::-webkit-scrollbar {
+    width: .2em;
+    height: .3rem;
+}
+
+.wraper-table::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0);
+}
+
+.wraper-table::-webkit-scrollbar-thumb {
+    background-color: #d2d2d2;
+    border-radius: 20px;
+}
+
 table{
     max-width:768px;
     border-spacing: 0;
@@ -23,5 +37,12 @@ table thead th{
 
 .product-column{
     width:250px;
+}
+
+@media (min-width: 1020px){
+    table{
+        width:100%;
+        max-width:none;
+    }
 }
 `
