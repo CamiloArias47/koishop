b67a81b70a6890c179cdcce51a58705f36eb50d3
Camilo arias <camillo47@gmail.com>
2022-01-12 20:30:47 -0500

    test ger variables from produccion

--- pages/api/eventsdev.js
+++ pages/api/eventsdev.js
@@ -18,67 +18,67 @@ const WOMPI_STATES = {
 
 export default async (request, response) => {
   const { query, body } = request
-  // const {
-  //    event,
-  //    data,
-  //    environment,
-  //    signature,
-  //    timestamp,
-  //    sent_at
-  //  } = body
+  const {
+     event,
+     data,
+     environment,
+     signature,
+     timestamp,
+     sent_at
+   } = body
 
   
-  let data = {
-    "transaction": {
-      "id": "114028-1641953792-17900",
-      "status": "APPROVED",
-      "currency": "COP",
-      "reference": "6phHHle2Qmq3qgfyB1Ho",
-      "created_at": "2022-01-12T02:16:32.854Z",
-      "billing_data": null,
-      "finalized_at": "2022-01-12T02:16:33.000Z",
-      "redirect_url": "https://koishop.vercel.app/success",
-      "customer_data": {
-        "legal_id": "11111111",
-        "full_name": "ANDRES CAMILO ARIAS MARTINEZ",
-        "phone_number": "+573991111111",
-        "legal_id_type": "CC"
-      },
-      "customer_email": "andres.camilo.arias@correounivalle.edu.co",
-      "payment_method": {
-        "type": "NEQUI",
-        "extra": {
-          "transaction_id": "SANDBOX-1641953793Twbxsd",
-          "external_identifier": "1641953793Y8o5lt"
-        },
-        "phone_number": "3991111111"
-      },
-      "status_message": null,
-      "amount_in_cents": 2000000,
-      "payment_link_id": null,
-      "shipping_address": {
-        "city": "Jamundi",
-        "region": "Cauca",
-        "country": "CO",
-        "phone_number": "33333333",
-        "address_line_1": "caloto 34"
-      },
-      "payment_source_id": null,
-      "payment_method_type": "NEQUI"
-    }
-  }
-  let event ="transaction.updated"
-  let environment = "test"
-  let signature = {
-      "checksum": "ac238e5ef5d914cb98ea8a1bf674cf5f6e9a29112440714d6fea5284cbe2f88f",
-      "properties": [
-        "transaction.id",
-        "transaction.status",
-        "transaction.amount_in_cents"
-      ]
-    }
-  let timestamp = 1641953793 
-  let sent_at = "2022-01-12T02:16:33.091Z"
+  // let data = {
+  //   "transaction": {
+  //     "id": "114028-1641953792-17900",
+  //     "status": "APPROVED",
+  //     "currency": "COP",
+  //     "reference": "6phHHle2Qmq3qgfyB1Ho",
+  //     "created_at": "2022-01-12T02:16:32.854Z",
+  //     "billing_data": null,
+  //     "finalized_at": "2022-01-12T02:16:33.000Z",
+  //     "redirect_url": "https://koishop.vercel.app/success",
+  //     "customer_data": {
+  //       "legal_id": "11111111",
+  //       "full_name": "ANDRES CAMILO ARIAS MARTINEZ",
+  //       "phone_number": "+573991111111",
+  //       "legal_id_type": "CC"
+  //     },
+  //     "customer_email": "andres.camilo.arias@correounivalle.edu.co",
+  //     "payment_method": {
+  //       "type": "NEQUI",
+  //       "extra": {
+  //         "transaction_id": "SANDBOX-1641953793Twbxsd",
+  //         "external_identifier": "1641953793Y8o5lt"
+  //       },
+  //       "phone_number": "3991111111"
+  //     },
+  //     "status_message": null,
+  //     "amount_in_cents": 2000000,
+  //     "payment_link_id": null,
+  //     "shipping_address": {
+  //       "city": "Jamundi",
+  //       "region": "Cauca",
+  //       "country": "CO",
+  //       "phone_number": "33333333",
+  //       "address_line_1": "caloto 34"
+  //     },
+  //     "payment_source_id": null,
+  //     "payment_method_type": "NEQUI"
+  //   }
+  // }
+  // let event ="transaction.updated"
+  // let environment = "test"
+  // let signature = {
+  //     "checksum": "ac238e5ef5d914cb98ea8a1bf674cf5f6e9a29112440714d6fea5284cbe2f88f",
+  //     "properties": [
+  //       "transaction.id",
+  //       "transaction.status",
+  //       "transaction.amount_in_cents"
+  //     ]
+  //   }
+  // let timestamp = 1641953793 
+  // let sent_at = "2022-01-12T02:16:33.091Z"
 
    const webhook = {
     event,
