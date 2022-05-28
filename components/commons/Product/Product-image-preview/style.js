0bb70a837ed5c91f97a242c660e495736e825b32
Camilo arias <camillo47@gmail.com>
2022-05-28 18:59:21 -0500

    change main image of product in product view 🖼🔁

--- components/commons/Product/Product-image-preview/style.js
+++ components/commons/Product/Product-image-preview/style.js
@@ -4,22 +4,41 @@ import { colors } from 'styles/theme'
 export default css`
 .preview{
     display:flex;
-    padding:.5rem;
+    padding:.1rem 0;
     width:100%;
 }
 .preview__element{
-    display: flex;
+    display: block;
     align-content: center;
-    border-radius: 50%;
-    border: 2px solid ${ colors.primary };
-    background-color: ${ colors.primary };
+    width: 20%;
+    padding: .1rem;
+    border: 0;
 }
+.preview__element:hover{
+    cursor:pointer;
+}
+.preview > .preview__element{
+    padding-left:0;
+}
+.preview__element:last-child{
+    padding-right:0;
+}
+
 .preview__element :global(img){
-    border-radius: 50%;
+    aspect-ratio: 1;
 }
 @media (min-width: 1020px){
     .preview{
-        width:60%;
+        width:19.5%;
+        flex-direction:column;
     } 
+    .preview__element{
+        width: 100%;
+    }
+}
+@media (min-width: 1200px){
+    .preview{
+        width: 14.8%;
+    }
 }
 `
