aead8371d254f7febd06c53af3b247c134e00fb7
Camilo arias <camillo47@gmail.com>
2022-08-05 17:33:00 -0500

    create payment result page 💵

--- firebaseApi/firestoreDB/webhook.js
+++ firebaseApi/firestoreDB/webhook.js
@@ -1,5 +1,5 @@
 import db from './db'
-import { query, collection, where, getDocs} from "firebase/firestore"
+import { query, collection, where, getDocs, doc, onSnapshot } from "firebase/firestore"
 
 export async function getWebhook({id}){
     let webhook = {}
@@ -7,6 +7,13 @@ export async function getWebhook({id}){
     const q = query(webhookCollection, where('data.transaction.id','==', id) )
     const docs = await getDocs(q);
 
-    docs.forEach( doc => webhook = doc.data() )
+    docs.forEach( doc => webhook = {'id':doc.id, ...doc.data()} )
     return webhook
+}
+
+export function watchWebhook(id, cb){
+    console.log({id})
+    return onSnapshot(doc(db, "webhooks", id), (doc) => {
+        cb({'id':doc.id, ...doc.data()});
+    });
 }
--- pages/user/pedidos/success.js
+++ pages/user/pedidos/success.js
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Image from 'next/image'
 import UserLayout from 'components/commons/UserLayout'
-import { getWebhook } from 'firebaseApi/firestoreDB/webhook'
+import SuccessAnimation from 'components/icons/SuccessAnimation'
+import { Spinner } from 'components/icons'
+import { watchWebhook } from 'firebaseApi/firestoreDB/webhook'
+import { TRANSACTION_STATUS } from 'components/CommerceContext'
+import creditCardLeft from 'public/images/card-fail-left.png'
+import creditCardRight from 'public/images/card-fail-right.png'
+import { colors } from 'styles/theme'
+import style from 'styles/style-success'
 
 export default function Success(){
     const router = useRouter()
@@ -9,20 +18,61 @@ export default function Success(){
     const [webhook, setWebhook] = useState({})
 
     useEffect( () => {
-        async function getterwebhook(){
-            let webhk = await getWebhook({id})
-            setWebhook(webhk)
+        function getterwebhook(){
+            let stop = watchWebhook(id, newUpdate => {
+                let { status } = newUpdate.data.transaction
+                if(status === TRANSACTION_STATUS.ok || status === TRANSACTION_STATUS.fail) stop()
+                setWebhook(newUpdate)
+            })
         } 
 
         if(id) getterwebhook()
     },[id])
 
+
     return(
         <UserLayout>
-            esta es la id
-            {id}<br/>
-            Este es el webhook:<br/>
-            {webhook.data?.transaction.status}
+            <section className='state-section'>
+                { 
+                    webhook.data && webhook.data.transaction.status === TRANSACTION_STATUS.ok
+                        ?   
+                            <>
+                                <h1>¡Transacción exitosa!</h1>
+                                <SuccessAnimation/>
+                            </>
+                        : 
+                        webhook.data && webhook.data.transaction.status === TRANSACTION_STATUS.fail
+                        ?
+                            <>
+                                <h1>Transacción rechazada</h1>
+                                <div className='animation'>
+                                    <div className='animation__card animation__card--left'>
+                                        <Image src={creditCardLeft} alt="tarjeta rota" layout="responsive" />
+                                    </div>
+                                    <div className='animation__card animation__card--right'>
+                                        <Image src={creditCardRight} alt="tarjeta rota" layout="responsive" />
+                                    </div>
+                                    <div className='animation__circle'></div>
+                                </div>
+                            </>
+                        :
+                            <>
+                                <h1>Transacción en proceso</h1>
+                                <Spinner width={40} height={40} color={colors.primary}/>
+                            </>
+                }
+
+                {
+                    webhook.data && webhook.data.transaction.status != TRANSACTION_STATUS.incomplete
+                     ?
+                        <Link href="/user/pedidos">
+                            <a>ir a tus pedidos</a>
+                        </Link>
+                     : ""
+                }
+
+            </section>
+            <style jsx>{style}</style>
         </UserLayout>
     )
 }
