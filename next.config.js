c52ea966445527145c03bf5f6885268dcd7294df
Camilo arias <camillo47@gmail.com>
2022-11-17 17:56:59 -0500

    add new domain for images 📷

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ module.exports = {
               'lh3.googleusercontent.com',
               'graph.facebook.com',
               'firebasestorage.googleapis.com',
-              'scontent.cdninstagram.com'
+              'scontent.cdninstagram.com',
+              'scontent-sea1-1.cdninstagram.com'
              ],
   },
 }
