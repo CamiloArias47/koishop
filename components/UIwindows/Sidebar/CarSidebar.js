063b3444b93a148db62d73f8217c990921987a27
Camilo Arias <camillo47@gmail.com>
2023-02-08 22:55:01 -0500

    add message in shopping cart empty 📥

--- components/UIwindows/Sidebar/CarSidebar.js
+++ components/UIwindows/Sidebar/CarSidebar.js
@@ -1,31 +1,34 @@
 import Link from "next/link"
 import { useCommerce } from "components/CommerceContext"
 import { formatPrice } from "utils"
-
+import EmptyCart from "components/commons/ItemCart/empty-cart"
 import ItemCart from 'components/commons/ItemCart'
 import style from './style-cart'
 
 export const CarSidebar = () => {
     const { cart, subtotalToPay } = useCommerce()
 
-    const btnCheckout = cart.length === 0 
-                            ? ''
-                            : <Link href="/pagar">
-                                <a className="btn btn-primary">Finalizar compra</a>
-                              </Link>
+    let btnCheckout = ''
+    let productsList = <EmptyCart/>
+
+    if(cart.length > 0 ){
+        btnCheckout = <Link href="/pagar">
+                         <a className="btn btn-primary">Finalizar compra</a>
+                      </Link>
+        productsList = cart.map( product => <ItemCart 
+                                                key={product.id} 
+                                                id={product.id}
+                                                name={product.name}
+                                                photo={product.photo}
+                                                cantidad={product.buyAmount}
+                                                price={product.price}
+                                            />)
+    }
 
     return (
         <div className="cart-container">
             <ul className="cart-list">
-                {cart.map( product => <ItemCart 
-                                        key={product.id} 
-                                        id={product.id}
-                                        name={product.name}
-                                        photo={product.photo}
-                                        cantidad={product.buyAmount}
-                                        price={product.price}
-                                       /> 
-                )}
+                {productsList}
             </ul>
             <div className="pay-resume">
                 <div className="pay-resume-total">
---
+++ components/icons/empty-cart.jsx
@@ -0,0 +1,20 @@
+const EmptyBagIcon = (props) => (
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-shopping-bag"
+    width={24}
+    height={24}
+    strokeWidth={2}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M6.331 8H17.67a2 2 0 0 1 1.977 2.304l-1.255 8.152A3 3 0 0 1 15.426 21H8.574a3 3 0 0 1-2.965-2.544l-1.255-8.152A2 2 0 0 1 6.331 8z" />
+    <path d="M9 11V6a3 3 0 0 1 6 0v5" />
+  </svg>
+)
+
+export default EmptyBagIcon
